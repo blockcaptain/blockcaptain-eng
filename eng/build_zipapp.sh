18bdb4d07958fd9efc6de3c2e644b7caaac86f15
Will Brown <will@rebeagle.com>
2021-01-28 03:04:14 +0000

    actions

---
+++ eng/build_zipapp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+DEST_DIR="build/contents"
+rm -rf "${DEST_DIR}"
+mkdir -p "${DEST_DIR}"
+pip install -r "requirements.txt" --no-deps "./" --target "${DEST_DIR}"
+python3 -m zipapp -c -m "blkcapteng.__main__:main" -p "/usr/bin/env python3" -o build/blkcapteng $DEST_DIR 
+chmod +x build/blkcapteng
